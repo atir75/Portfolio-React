9209d838a9492f2ebaef8336dcb058a90d80bf12
atir75 <maria.rita27012@gmail.com>
2023-01-30 23:23:46 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { Briefcase, Envelope, Eye, House, User } from "phosphor-react";
+import "../Header/styles.css";
+
+export function Header() {
+  return (
+    <div>
+      <header className="Header">
+        <div className="HeaderIcon">
+            <a><House size={32}/></a>
+            <a><User size={32} /></a>
+            <a><Eye size={32} /></a>
+            <a><Briefcase size={32} /></a>
+            <a><Envelope size={32} /></a>
+        </div>
+      </header>
+    </div>
+  );
+}
