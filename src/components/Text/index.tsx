ff7594cef1296c0ef28c9602356f9ad7e1641cd1
atir75 <maria.rita27012@gmail.com>
2023-02-03 00:24:19 -0300

    commit text css

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,6 +2,6 @@ import "../Text/styles.css";
 
 export function Text() {
     return (
-        <h1>Hi, <p>I’m Maria Rita </p>Fullstack Developer.</h1>
+        <h1>Hi, <p>I’m Maria Rita</p>Fullstack Developer.</h1>
     );
 }
