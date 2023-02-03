46b66b4a5b7114e38abf1c905edb38fe0b2ea584
atir75 <maria.rita27012@gmail.com>
2023-02-03 20:39:50 -0300

    feat(*): created folder img

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
 import { Cube1 } from './components/Cube1'
+import { Cube2 } from './components/Cube2'
 import { Header } from './components/Header'
 import { Text } from './components/Text'
 
 export function App() {
 
   return (
-    <><Header></Header><Text></Text><Cube1></Cube1></>
+    <><Header></Header><Text></Text><Cube1></Cube1><Cube2></Cube2></>
   )
 }
---
+++ src/components/Cube2/index.tsx
@@ -0,0 +1,15 @@
+import "../Cube2/styles.css";
+
+
+export function Cube2() {
+    return(
+        <div className="Cube2">
+            <div className="face2 face--front2">Front</div>
+            <div className="face2 face--right2">right</div>
+            <div className="face2 face--back2">Back</div>
+            <div className="face2 face--left2">Left</div>
+            <div className="face2 face--top2">Top</div>
+            <div className="face2 face--bottom2">Bottom</div>
+        </div>
+    )
+}
