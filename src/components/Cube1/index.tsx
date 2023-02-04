856e4aecbd5867a51e70585b261192486ccbdaf4
atir75 <maria.rita27012@gmail.com>
2023-02-04 19:09:04 -0300

    wip(cubes): cube1 finished

--- src/components/Cube1/index.tsx
+++ src/components/Cube1/index.tsx
@@ -10,11 +10,36 @@ export function Cube1() {
                     src="https://growiz.com.br/wp-content/uploads/2020/08/kisspng-c-programming-language-logo-microsoft-visual-stud-atlas-portfolio-5b899192d7c600.1628571115357423548838.png">
                 </img>
             </div>
-            <div className="face face--right">right</div>
-            <div className="face face--back">Back</div>
-            <div className="face face--left">Left</div>
-            <div className="face face--top">Top</div>
-            <div className="face face--bottom">Bottom</div>
+            <div className="face face--right">
+                <img
+                className="java"
+                    src="https://www.celsonunes.com.br/wp-content/uploads/2018/05/java-logo.png">    
+                </img>
+            </div>
+            <div className="face face--back">
+                <img
+                className="python"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png">
+                </img>
+            </div>
+            <div className="face face--left">
+            <img
+                className="git"
+                    src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png">
+                </img>
+            </div>
+            <div className="face face--top">
+            <img
+                className="nodejs"
+                    src="https://www.openxcell.com/wp-content/uploads/2021/11/nodejs-inner.png">
+            </img>
+            </div>
+            <div className="face face--bottom">
+            <img
+                className="nodejs"
+                    src="https://www.openxcell.com/wp-content/uploads/2021/11/nodejs-inner.png">
+            </img>
+            </div>
         </div>
     )
 }
