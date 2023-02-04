8a4935cc42086ebe6db1542186559c191a24a563
atir75 <maria.rita27012@gmail.com>
2023-02-03 23:17:34 -0300

    fix(*): fixed the cube rotation

--- src/components/Cube1/index.tsx
+++ src/components/Cube1/index.tsx
@@ -4,7 +4,12 @@ import "../Cube1/styles.css";
 export function Cube1() {
     return(
         <div className="cube2">
-            <div className="face face--front">Front</div>
+            <div className="face face--front">
+                <img
+                className="csharp"
+                    src="https://growiz.com.br/wp-content/uploads/2020/08/kisspng-c-programming-language-logo-microsoft-visual-stud-atlas-portfolio-5b899192d7c600.1628571115357423548838.png">
+                </img>
+            </div>
             <div className="face face--right">right</div>
             <div className="face face--back">Back</div>
             <div className="face face--left">Left</div>
