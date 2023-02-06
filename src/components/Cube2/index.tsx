35ea881f8fccee417d4f2d2155ae95c284a5ea0f
atir75 <maria.rita27012@gmail.com>
2023-02-06 16:00:42 -0300

    wip(Cubes): second cube

--- src/components/Cube2/index.tsx
+++ src/components/Cube2/index.tsx
@@ -9,11 +9,34 @@ export function Cube2() {
                     src="https://fslightbox.com/img/environments/fix/react.png">
                 </img>
             </div>
-            <div className="face2 face--right2">right</div>
-            <div className="face2 face--back2">Back</div>
-            <div className="face2 face--left2">Left</div>
-            <div className="face2 face--top2">Top</div>
+            <div className="face2 face--right2">
+            <img 
+                className="js"
+                    src="https://logospng.org/download/javascript/logo-javascript-1024.png">
+                </img>
+            </div>
+            <div className="face2 face--back2">
+                <img 
+                className="angular"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png">
+                </img>
+            </div>
+            <div className="face2 face--left2">
+            <img 
+                className="css"
+                    src="https://logospng.org/download/css-3/logo-css-3-2048.png">
+                </img>
+            </div>
+            <div className="face2 face--top2">
+                <img 
+                className="sass"
+                    src="https://mateusmedeiros.com/assets/img/posts/logo-sass.png">
+                </img>
+            </div>
             <div className="face2 face--bottom2">Bottom</div>
         </div>
     )
 }
+
+
+//https://mateusmedeiros.com/assets/img/posts/logo-sass.png
