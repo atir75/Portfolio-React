37afbd6f235fd9565016f9246c571796ce7dd01a
atir75 <maria.rita27012@gmail.com>
2023-02-11 20:49:06 -0300

    wip(aboutme): text1

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import { Cube2 } from './components/Cube2'
 import { Header } from './components/Header'
 import { Text } from './components/Text'
 import { BtnContactMe } from './components/Button ContactMe'
+import { AboutMe } from './components/AboutMe'
 
 export function App() {
 
   return (
-    <><Header></Header><Text></Text><Cube1></Cube1><Cube2></Cube2><BtnContactMe></BtnContactMe></>
+    <><Header></Header><Text></Text><Cube1></Cube1><Cube2></Cube2><BtnContactMe></BtnContactMe><AboutMe></AboutMe></>
   )
 }
---
+++ src/components/AboutMe/index.tsx
@@ -0,0 +1,11 @@
+import "../AboutMe/styles.css";
+
+export function AboutMe() {
+    return (
+        <><h1 className="title">About Me</h1>
+        <h2 className="text1">I´ve been in the area more than 1 year,<p>
+My studies started in 2021 at Santa </p>Cecilia university</h2>
+        </>
+
+    )
+}
