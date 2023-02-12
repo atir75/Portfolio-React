bb7c162b866b30801849b0317ea04239936bad2a
atir75 <maria.rita27012@gmail.com>
2023-02-11 22:03:53 -0300

    wip(aboutme): text2

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -3,8 +3,9 @@ import "../AboutMe/styles.css";
 export function AboutMe() {
     return (
         <><h1 className="title">About Me</h1>
-        <h2 className="text1">I´ve been in the area more than 1 year,<p>
+        <h2 className="text1">I´ve been in the area more than 1 year,<p className="lineABT">
 My studies started in 2021 at Santa </p>Cecilia university</h2>
+        <h2 className="text2">Also, I have studied by <p className="lineABT2">my way, both technology and </p>another languages</h2>
         </>
 
     )
