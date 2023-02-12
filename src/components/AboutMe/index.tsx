4d889fce213d8a748ad8ac9fa0fa1f41bf9d493d
atir75 <maria.rita27012@gmail.com>
2023-02-11 22:06:39 -0300

    wip(aboutme): text3

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -6,6 +6,7 @@ export function AboutMe() {
         <h2 className="text1">I´ve been in the area more than 1 year,<p className="lineABT">
 My studies started in 2021 at Santa </p>Cecilia university</h2>
         <h2 className="text2">Also, I have studied by <p className="lineABT2">my way, both technology and </p>another languages</h2>
+        <h2 className="text3">my hobbies are doing puzzles, reading books and playing games</h2>
         </>
 
     )
