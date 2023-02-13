d762d8b3a3fcbe0b1a9d3eba0a598010e25c7ad7
atir75 <maria.rita27012@gmail.com>
2023-02-13 15:40:03 -0300

    wip(items): created

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import { Header } from './components/Header'
 import { Text } from './components/Text'
 import { BtnContactMe } from './components/Button ContactMe'
 import { AboutMe } from './components/AboutMe'
+import { ItemsAboutMe } from './components/ItemsAboutMe'
 
 export function App() {
 
   return (
-    <><Header></Header><Text></Text><Cube1></Cube1><Cube2></Cube2><BtnContactMe></BtnContactMe><AboutMe></AboutMe></>
+    <><Header></Header><Text></Text><Cube1></Cube1><Cube2></Cube2><BtnContactMe></BtnContactMe><AboutMe></AboutMe><ItemsAboutMe></ItemsAboutMe></>
   )
 }
---
+++ src/components/ItemsAboutMe/index.tsx
@@ -0,0 +1,10 @@
+import "../ItemsAboutMe/styles.css"
+
+export function Items() {
+    return (
+        <>
+        <h1>aaaasaaaaaaaaaaaaaaaaaaaa</h1>
+        <img src="../assets/img/mew"></img>
+        </>
+    )
+}
