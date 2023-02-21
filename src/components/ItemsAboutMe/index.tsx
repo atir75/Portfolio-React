c494d5baa898183cd7a67705391b3a6fa37552ee
atir75 <maria.rita27012@gmail.com>
2023-02-21 14:26:00 -0300

    wip(*): resize the puzzles

--- src/components/ItemsAboutMe/index.tsx
+++ src/components/ItemsAboutMe/index.tsx
@@ -1,12 +1,20 @@
 import "../ItemsAboutMe/styles.css"
 import mew from "../../../assets/img/mew.png"
+import puzzle1 from "../../../assets/img/puzzle1.png"
+import puzzle2 from "../../../assets/img/puzzle2.png"
 
 export function Items() {
     return (
         <>
-        <img
-            className="mew" src={ mew }>
-        </img>
+            <img
+                className="mew" src={ mew }>
+            </img>
+            <img
+                className="puzzle1" src={ puzzle1 }>
+            </img>
+            <img
+                className="puzzle2" src={ puzzle2 }>
+            </img>
         </>
     )
 }
