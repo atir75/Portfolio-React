ca0944452e338669eb8c1eccfa9084f1fbc2b507
atir75 <maria.rita27012@gmail.com>
2023-02-21 18:38:48 -0300

    wip(Items): puzzle 2 position

--- src/components/ItemsAboutMe/index.tsx
+++ src/components/ItemsAboutMe/index.tsx
@@ -2,6 +2,8 @@ import "../ItemsAboutMe/styles.css"
 import mew from "../../../assets/img/mew.png"
 import puzzle1 from "../../../assets/img/puzzle1.png"
 import puzzle2 from "../../../assets/img/puzzle2.png"
+import controle2 from "../../../assets/img/controle2.png"
+import me from "../../../assets/img/me.png"
 
 export function Items() {
     return (
@@ -13,7 +15,13 @@ export function Items() {
                 className="puzzle1" src={ puzzle1 }>
             </img>
             <img
+                className="controle2" src={ controle2 }>
+            </img>
+            <img
                 className="puzzle2" src={ puzzle2 }>
+            </img>
+            <img
+                className="me" src={ me }>
             </img>
         </>
     )
