43e8bcb0bc5971c5e0d90be79e558e380bea2511
atir75 <maria.rita27012@gmail.com>
2023-03-03 00:22:58 -0300

    fix(Skills): position

--- src/components/Skilss/index.tsx
+++ src/components/Skilss/index.tsx
@@ -3,6 +3,8 @@ import "../Skilss/styles.css"
 export function Skills() {
     return (
         <>
+            <h1 className="TextSkills">Skills</h1>
+            <h2 className="Backend">Back-end</h2>
             <div className="progress-bar" />
         </>
     )
