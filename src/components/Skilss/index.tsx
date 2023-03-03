84d51b3acbb5383243cfdfc9a41f91706ef8b668
atir75 <maria.rita27012@gmail.com>
2023-03-03 14:05:45 -0300

    wip(Skilss): bars created

--- src/components/Skilss/index.tsx
+++ src/components/Skilss/index.tsx
@@ -3,10 +3,17 @@ import "../Skilss/styles.css"
 export function Skills() {
     return (
         <>
+
+
             <h1 className="TextSkills">Skills</h1>
             <h2 className="Backend">Back-end</h2>
-            <div className="back1">C#</div>
-            <div className="progress-bar"></div> 
+            <div className="ProgressBars">
+                <div className="progress-bar"></div> 
+                <div className="progress-bar"></div> 
+                <div className="progress-bar"></div> 
+                <div className="progress-bar"></div> 
+                <div className="progress-bar"></div> 
+            </div>
         </>
     )
 }
