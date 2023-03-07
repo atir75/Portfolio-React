d6dddf009abbfc55c2d693d8c56f2fbeed183743
atir75 <maria.rita27012@gmail.com>
2023-03-07 02:06:01 -0300

    wip(skills): name bars

--- src/components/Skilss/index.tsx
+++ src/components/Skilss/index.tsx
@@ -8,8 +8,12 @@ export function Skills() {
             <h1 className="TextSkills">Skills</h1>
             <h2 className="Backend">Back-end</h2>
             <div className="ProgressBars">
-                <div className="progress-bar progressAnimationStrike one" ></div> 
-                <div className="progress-bar progressAnimationStrike two"></div> 
+                <div className="progress-bar progressAnimationStrike one" >
+                    <h2 className="first">C#</h2>
+                </div> 
+                <div className="progress-bar progressAnimationStrike two">
+                    <h2 className="second">NodeJs</h2>
+                </div> 
                 <div className="progress-bar progressAnimationStrike three"></div> 
                 <div className="progress-bar progressAnimationStrike four"></div> 
                 <div className="progress-bar progressAnimationStrike five"></div> 
