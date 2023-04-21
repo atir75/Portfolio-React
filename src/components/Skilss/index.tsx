345eb8976753f3f21d05a0ec44093bb97e73184a
Maria Rita <101608517+atir75@users.noreply.github.com>
2023-04-21 15:27:09 -0300

    wip: frontend skilss

--- src/components/Skilss/index.tsx
+++ src/components/Skilss/index.tsx
@@ -6,11 +6,13 @@ export function Skills() {
 
 
             <h1 className="TextSkills">Skills</h1>
+            <h2 className="Frontend">Front-end</h2>
             <h2 className="Backend">Back-end</h2>
-            <div className="teste">  .</div>
+            
+            <div className="teste"></div>
 
 
-            <div className="wrap-circles">
+            <div className="wrap-circlesback">
                 <div className="circle per-0">
                     <div className="inner">CSharp</div>
                 </div>
@@ -21,6 +23,22 @@ export function Skills() {
                     <div className="inner">Java</div>
                 </div>
                 <div className="circle per-75">
+                    <div className="inner">Git</div>
+                </div>
+            </div>
+
+
+            <div className="wrap-circlesfront">
+                <div className="circle per-0f">
+                    <div className="inner">React</div>
+                </div>
+                <div className="circle per-25f">
+                    <div className="test">JavaScript</div>
+                </div>
+                <div className="circle per-50f">
+                    <div className="inner">Java</div>
+                </div>
+                <div className="circle per-75f">
                     <div className="inner">Git</div>
                 </div>
             </div>
